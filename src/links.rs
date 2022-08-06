1b8965afddc499ab6a99a67c7e97c46ad50e5d2d
damien <doute.d@gmail.com>
2022-08-05 20:44:16 -0400

    Simplified to LinkType

--- src/links.rs
+++ src/links.rs
@@ -16,15 +16,12 @@ pub fn get_links_details(links: &mut Vec<Link>, option: GetOptions) -> ExternRes
 }
 
 ///
-pub fn get_typed_from_links<R: TryFrom<Entry>, E>(
+pub fn get_typed_from_links<R: TryFrom<Entry>>(
    base: EntryHash,
-   link_type: impl TryInto<LinkTypeRanges, Error = E>,
+   link_type: LinkType,
    tag: Option<LinkTag>,
    //include_latest_updated_entry: bool,
-) -> ExternResult<Vec<(R, Link)>>
-where
-   WasmError: From<E>
-{
+) -> ExternResult<Vec<(R, Link)>> {
    let links = get_links(AnyLinkableHash::from(base), link_type, tag)?;
    //debug!("get_links_and_load_type() links found: {}", links.len());
    let result_pairs = get_links_details(&mut links.clone(), GetOptions::default())?;
