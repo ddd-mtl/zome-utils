3535d549e62eab391d63d982d5213872881009da
damien <doute.d@gmail.com>
2023-07-11 16:17:47 -0400

    Updated create_link_relaxed() to holochain 0.1.3

--- src/relaxed.rs
+++ src/relaxed.rs
@@ -26,17 +26,25 @@ pub fn create_entry_relaxed<I, E, E2>(typed: I) -> ExternResult<ActionHash>
 
 
 ///
-pub fn create_link_relaxed<T: Into<LinkTag>>(
-   base_address: EntryHash,
-   target_address: EntryHash,
-   link_type: LinkType,
-   tag: T,
-) -> ExternResult<ActionHash> {
+pub fn create_link_relaxed<T, E>(
+   base_address: impl Into<AnyLinkableHash>,
+   target_address: impl Into<AnyLinkableHash>,
+   link_type: T,
+   tag: impl Into<LinkTag>,
+) -> ExternResult<ActionHash>
+   where
+      ScopedLinkType: TryFrom<T, Error = E>,
+      WasmError: From<E>,
+{
+   let ScopedLinkType {
+      zome_index,
+      zome_type: link_type,
+   } = link_type.try_into()?;
    HDK.with(|h| {
       h.borrow().create_link(CreateLinkInput::new(
          base_address.into(),
          target_address.into(),
-         zome_info()?.id,
+         zome_index,
          link_type,
          tag.into(),
          ChainTopOrdering::Relaxed,
